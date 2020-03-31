6b2586fae565f0b6d1a3e13be9227da494d66a8e
unknown <prakhar2801@gmail.com>
2020-04-01 00:28:57 +0530

    first commit

--- learning_templates/basic_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from basic_app import views
-from django.urls import path
-
-app_name='basic_app'
-urlpatterns = [
-    path('relative/',views.relative,name='relative'),
-    path('other/', views.other,name='other'),
-]
